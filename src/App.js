d558626c8c33794db7ce926fb7449c509b968d3f
kamykov <krzysztof@thinkstudio.pl>
2018-10-18 09:05:50 +0200

    store setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { createStore } from 'redux';
 import './App.sass';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
+import reducer from './reducers';
+import Simple from './components/Simple';
 
-        </header>
-      </div>
-    );
-  }
-}
+const initialState = { tech: 'initialReducer' };
+const store = createStore(
+  reducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <Simple tech={store.getState().tech} />
+    </header>
+  </div>
+);
 
 export default App;
---
+++ src/components/Simple.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Simple(props) {
+  console.log(props);
+  const { tech } = props;
+
+  return (
+    <div>
+simple :
+      {tech}
+    </div>
+  );
+}
+
+Simple.propTypes = {
+  tech: PropTypes.string.isRequired,
+};
